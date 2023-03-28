d4ae0f867c93979fc6190515106c367d0a21cad5
ImoH <imogen@hay.cx>
2023-03-28 15:22:03 +0100

    CGEMA-22 Remove comment

--- Event-Management-Service/config/default.ts
+++ Event-Management-Service/config/default.ts
@@ -1,5 +1,3 @@
 export default {
-    port: 1337,
-    // dbUri: "mongodb://localhost:27017/events",
     dbUri: "mongodb+srv://event-management.bsw5xrq.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority",
 };
