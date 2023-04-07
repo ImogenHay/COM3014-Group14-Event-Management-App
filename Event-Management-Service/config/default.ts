982c08f93fc58db26dea1b24d3f1647c827be149
Em-Solo <solomonidese2001@gmail.com>
2023-04-07 20:08:53 +0100

    Defined the port in default and finalised the routes

--- Event-Management-Service/config/default.ts
+++ Event-Management-Service/config/default.ts
@@ -1,3 +1,4 @@
 export default {
+    port: "3001",
     dbUri: "mongodb+srv://event-management.bsw5xrq.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority",
 };
--- Event-Management-Service/src/routes.ts
+++ Event-Management-Service/src/routes.ts
@@ -1,9 +1,10 @@
 import { type Express, type Request, type Response } from 'express'
 import {
+  bookEventTicketsHandler, checkEventAvailabilityHandler,
+  createEventHandler, deleteEventHandler,
   getAllEventsHandler,
-  getCheckAvailabilityHandler, getEventHandler,
-  postCreateEventHandler,
-  putBookTicketHandler
+  getEventHandler, updateEventHandler,
+
 } from './Controllers/events_controller'
 
 // creating a function that takes in a parameter of type expresss
@@ -14,10 +15,10 @@ function routes (app: Express) {
   app.post('/events/create', createEventHandler)
   app.get('/events', getAllEventsHandler)
   app.get('/events/:eventId', getEventHandler)
-  app.put('events/:eventId' updateEventHandler)
-  app.delete('events/:eventId' deleteEventHandler)
-  app.get('/events/check/:eventId', checkAvailabilityHandler)
-  app.put('/events/book-ticket/:eventId/:numOfTickets', bookTicketHandler)
+  app.put('events/:eventId', updateEventHandler)
+  app.delete('events/:eventId', deleteEventHandler)
+  app.get('/events/check/:eventId', checkEventAvailabilityHandler)
+  app.put('/events/book-ticket/:eventId/:numOfTickets', bookEventTicketsHandler)
 }
 
 export default routes
