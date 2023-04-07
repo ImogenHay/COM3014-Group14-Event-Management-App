abffdf5a0b85de68e66bc3a6a2daf8b0b0f087b8
Em-Solo <solomonidese2001@gmail.com>
2023-04-07 15:32:39 +0100

    Fixed bug int logger, improved connect and refactored app to more efficient layout

--- Event-Management-Service/src/Utils/connect.ts
+++ Event-Management-Service/src/Utils/connect.ts
@@ -6,10 +6,17 @@ const ca = 'config/X509-cert.pem'
 
 const options: mongoose.ConnectOptions = {
   sslKey: ca,
-  sslCert: ca
+  sslCert: ca,
+  serverSelectionTimeoutMS: 1000
 }
 
-async function connect () {
+// with the use of this interface an option is given to choose if the app terminates when the connection doesnt happen
+// usefull during development
+interface AppDatabaseConnectionOptions {
+  exitOnFailure?: boolean;
+}
+
+async function connect (connectionOptions: AppDatabaseConnectionOptions = {}) {
   const dbUri = config.get<string>('dbUri')
 
   try {
@@ -17,7 +24,9 @@ async function connect () {
     logger.info('Connected to MongoDB Atlas!')
   } catch (error) {
     logger.error('Could not connect to MongoDB Atlas')
-    process.exit(1)
+    // this is used with to indicate that an error occurred
+    // usually theres an exit code of 0
+    if (connectionOptions.exitOnFailure ?? true) process.exit(1);
   }
 }
 
