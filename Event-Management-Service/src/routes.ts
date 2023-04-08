d4ea360f7d7a52a415b2d3ebbb22733aa2ed7a60
Em-Solo <solomonidese2001@gmail.com>
2023-04-08 14:01:41 +0100

    Implemented the schema validation middlewear in the routes, created and implemented empty user validation middleawear in the routes

--- Event-Management-Service/src/routes.ts
+++ Event-Management-Service/src/routes.ts
@@ -6,19 +6,29 @@ import {
   getEventHandler, updateEventHandler,
 
 } from './Controllers/events_controller'
+import {validateUser} from "./Middleware/validateUser";
+import validateResource from "./Middleware/validateResource";
+import {
+  bookEventTicketsSchema,
+  checkEventAvailabilitySchema,
+  createEventSchema,
+  deleteEventSchema,
+  getEventSchema,
+  updateEventSchema
+} from "./Schemas/events_schemas";
 
 // creating a function that takes in a parameter of type expresss
 function routes (app: Express) {
   app.get('/events/healthcheck', (req: Request, res: Response) => {
     res.sendStatus(200)
   })
-  app.post('/events/create', createEventHandler)
-  app.get('/events', getAllEventsHandler)
-  app.get('/events/:eventId', getEventHandler)
-  app.put('events/:eventId', updateEventHandler)
-  app.delete('events/:eventId', deleteEventHandler)
-  app.get('/events/check/:eventId', checkEventAvailabilityHandler)
-  app.put('/events/book-ticket/:eventId/:numOfTickets', bookEventTicketsHandler)
+  app.post('/events/create', [validateUser, validateResource(createEventSchema)], createEventHandler)
+  app.get('/events', validateUser, getAllEventsHandler)
+  app.get('/events/:eventId', [validateUser, validateResource(getEventSchema)], getEventHandler)
+  app.put('events/:eventId', [validateUser, validateResource(updateEventSchema)], updateEventHandler)
+  app.delete('events/:eventId', [validateUser, validateResource(deleteEventSchema)],deleteEventHandler)
+  app.get('/events/check/:eventId', [validateUser, validateResource(checkEventAvailabilitySchema)], checkEventAvailabilityHandler)
+  app.put('/events/book-ticket/:eventId/:numOfTickets', [validateUser, validateResource(bookEventTicketsSchema)], bookEventTicketsHandler)
 }
 
 export default routes
