8f287ec2455c5eb38e78055e5d365deac5c33344
ImoH <imogen@hay.cx>
2023-04-24 16:24:08 +0100

    CGEMA-18 Resizeable event widgets

--- Interface-Service/src/pages/Home.jsx
+++ Interface-Service/src/pages/Home.jsx
@@ -8,6 +8,8 @@ import {
     HStack,
     Badge,
     Button,
+    Grid,
+    GridItem,
 } from '@chakra-ui/react';
 
 export default function Home() {
@@ -22,38 +24,49 @@ export default function Home() {
         fetchEvents();
     }, []);
 
+    function handleBookClick(eventId) {
+        // handle booking event
+    }
+
     return (
         <Box p={4}>
             <Heading as="h1" size="2xl" mb={4}>
                 Events
             </Heading>
-            <VStack spacing={8} align="stretch">
+            <Grid
+                templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)', lg: 'repeat(3, 1fr)' }}
+                gap={6}
+            >
                 {events.map((event) => (
-                    <Box key={event.eventId} borderWidth="1px" borderRadius="lg" p={4}>
-                        <Heading as="h2" size="lg" mb={2}>
-                            {event.name}
-                        </Heading>
-                        <Text mb={2}>{event.description}</Text>
-                        <HStack mb={2}>
-                            <Badge colorScheme="purple">Venue</Badge>
-                            <Text>{event.venue}</Text>
-                        </HStack>
-                        <HStack mb={2}>
-                            <Badge colorScheme="purple">Date</Badge>
-                            <Text>{event.date}</Text>
-                        </HStack>
-                        <HStack mb={2}>
-                            <Badge colorScheme="purple">Duration</Badge>
-                            <Text>{event.duration} hours</Text>
-                        </HStack>
-                        <HStack mb={2}>
-                            <Badge colorScheme="purple">Available Tickets</Badge>
-                            <Text>{event.availableTickets}</Text>
-                        </HStack>
-                        <Button colorScheme="purple">Book Tickets</Button>
-                    </Box>
+                    <GridItem key={event.eventId}>
+                        <Box borderWidth="1px" borderRadius="lg" p={4}>
+                            <Heading as="h2" size="md" mb={2}>
+                                {event.name}
+                            </Heading>
+                            <Text mb={2}>{event.description}</Text>
+                            <HStack mb={2}>
+                                <Badge colorScheme="purple">Venue</Badge>
+                                <Text>{event.venue}</Text>
+                            </HStack>
+                            <HStack mb={2}>
+                                <Badge colorScheme="purple">Date</Badge>
+                                <Text>{event.date}</Text>
+                            </HStack>
+                            <HStack mb={2}>
+                                <Badge colorScheme="purple">Duration</Badge>
+                                <Text>{event.duration} hours</Text>
+                            </HStack>
+                            <HStack mb={2}>
+                                <Badge colorScheme="purple">Available Tickets</Badge>
+                                <Text>{event.availableTickets}</Text>
+                            </HStack>
+                            <Button colorScheme="purple" onClick={() => handleBookClick(event.eventId)}>
+                                Book Tickets
+                            </Button>
+                        </Box>
+                    </GridItem>
                 ))}
-            </VStack>
+            </Grid>
         </Box>
     );
 }
