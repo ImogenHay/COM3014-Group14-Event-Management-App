10a9ed9ed92342b1a3ae265e5f58ae5de1858a5e
Em-Solo <solomonidese2001@gmail.com>
2023-05-01 19:13:50 +0100

    Finished basic funtionality of auth

--- Auth-service/src/app.js
+++ Auth-service/src/app.js
@@ -5,24 +5,25 @@ const cors = require('cors')
 const userRoutes = require('./routes')
 const mongoose = require('mongoose')
 
-(async () => {
-
+// ( async () =>{
 // configuring dotenv
 dotenv.config()
+// getting env variables
 const port = process.env.PORT
 const dbUri = process.env.dbURI
 
+// getting our certificate
+const ca = 'config/X509-cert.pem'
+
+// setting the database connection options
+const options = {
+    sslKey: ca,
+    sslCert: ca,
+    serverSelectionTimeoutMS: 1000
+}
+
 // Creation of our express app
 const app = express()
-
-//connecting to the database
-try {
-    await mongoose.connect(dbUri)
-    console.log('Connected to the database')
-} catch (error) {
-    console.log('Could not connect to the database')
-    console.log(error)
-}
 
 //middleware
 app.use(express.json())
@@ -38,8 +39,15 @@ app.use((req, res, next) => {
 // using the user routes defined in the express router
 app.use('/auth', userRoutes)
 
-// listening for requests
-app.listen(port, () => {
-    console.log(`Application listening at http://localhost:${port}`)
-})
-})()
+mongoose.connect(dbUri, options)
+    .then(() => {
+        // listen for requests
+        app.listen(port, () => {
+            console.log('Connected to the database')
+            console.log(`Application listening at http://localhost:${port}`)
+        })
+    })
+    .catch((error) => {
+        console.log('Could not connect to the database, application not listening')
+        console.log(error)
+    })
