08f0d74101798286429e175d9e6581f11d462fd1
ImoH <imogen@hay.cx>
2023-05-01 17:23:45 +0100

    CGEMA-17 Add Sign up and Sign in Pages and Logout Button

--- Interface-Service/src/App.jsx
+++ Interface-Service/src/App.jsx
@@ -1,35 +1,47 @@
 import Navbar from "./components/Navbar.jsx"
 import Home from "./pages/Home.jsx"
-import Authorise from "./pages/Authorise.jsx"
+import SignIn from "./pages/SignIn.jsx"
+import SignUp from "./pages/SignUp.jsx"
 import ManageEvents from "./pages/ManageEvents.jsx"
 import Tickets from "./pages/Tickets.jsx"
 import About from "./pages/About.jsx"
 import {ChakraProvider} from "@chakra-ui/react";
 import NewEventForm from "./components/NewEventForm.jsx";
+import { useAuthContext } from './hooks/useAuthContext.jsx'
+
 
 function App() {
+    const { user } = useAuthContext()
     let Pathway
-    switch (window.location.pathname) {
-        case "/":
-            Pathway = Home
-            break
-        case "/Authorise":
-            Pathway = Authorise
-            break
-        case "/ManageEvents":
-            Pathway = ManageEvents
-            break
-        case "/Tickets":
-            Pathway = Tickets
-            break
-        case "/About":
-            Pathway = About
-            break
+    if (user) {
+        switch (window.location.pathname) {
+            case "/":
+                Pathway = Home
+                break
+            case "/ManageEvents":
+                Pathway = ManageEvents
+                break
+            case "/Tickets":
+                Pathway = Tickets
+                break
+            case "/About":
+                Pathway = About
+                break
+        }
+    } else {
+            switch (window.location.pathname) {
+                case "/":
+                    Pathway = SignUp
+                    break
+                case "/SignIn":
+                    Pathway = SignIn
+                    break
+            }
     }
 
     return (
         <ChakraProvider>
-            <Navbar  />
+            {user && <Navbar />}
             <div className="container">
                 <Pathway  />
             </div>
---
+++ Interface-Service/src/pages/SignIn.jsx
@@ -0,0 +1,90 @@
+import { useState } from "react"
+import { useLogin } from "../hooks/useLogin"
+import {
+    Flex,
+    Box,
+    FormControl,
+    FormLabel,
+    Input,
+    Checkbox,
+    Stack,
+    Link,
+    Button,
+    Heading,
+    Text,
+    useColorModeValue,
+} from '@chakra-ui/react';
+
+export default function SignIn() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const {login, error, isLoading} = useLogin()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        await login(email, password)
+    }
+
+    return (
+        <Flex
+            minH={'100vh'}
+            align={'center'}
+            justify={'center'}
+            bg={useColorModeValue('gray.50', 'gray.800')}>
+            <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+                <Stack align={'center'}>
+                    <Heading fontSize={'4xl'}>Sign in to your account</Heading>
+                    <Text fontSize={'lg'} color={'gray.600'}>
+                        to enjoy all our cool events! ✌️
+                    </Text>
+                </Stack>
+                <Box
+                    rounded={'lg'}
+                    bg={useColorModeValue('white', 'gray.700')}
+                    boxShadow={'lg'}
+                    p={8}>
+                    <Stack spacing={4}>
+                        <FormControl id="email">
+                            <FormLabel>Email address</FormLabel>
+                            <Input
+                                type="email"
+                                onChange={(e) => setEmail(e.target.value)}
+                                value={email}
+                            />
+                        </FormControl>
+                        <FormControl id="password">
+                            <FormLabel>Password</FormLabel>
+                            <Input
+                                 type="password"
+                                 onChange={(e) => setPassword(e.target.value)}
+                                 value={password}
+                            />
+                        </FormControl>
+                        <Stack spacing={10}>
+                            <Stack
+                                direction={{ base: 'column', sm: 'row' }}
+                                align={'start'}
+                                justify={'space-between'}>
+                                <Checkbox>Remember me</Checkbox>
+                                <Link color={'blue.400'}>Forgot password?</Link>
+                            </Stack>
+                            <Button
+                                bg={'blue.400'}
+                                color={'white'}
+                                disabled={isLoading}
+                                _hover={{
+                                    bg: 'blue.500',
+                                }}
+                                onClick={handleSubmit}
+                            >
+                                Sign in
+                            </Button>
+                        </Stack>
+                    </Stack>
+                </Box>
+            </Stack>
+            {error && <div className="error">{error}</div>}
+        </Flex>
+    )
+}
